574bb921a69d9bdcdaf9d3c6432fa7629bee2010
kcy1860 <ssdut.1860@gmail.com>
2013-02-17 18:08:18 -0500

    new files

---
+++ compute_error.m
@@ -0,0 +1,33 @@
+function [error_rate] = compute_error(user_list,recommend_list)
+    
+    %user_list:user_id|movie_id|rating|timestamp
+    %recommend_list:user_id|recommend_movie_id|score
+   
+    threshold = 4;
+    error = 0;
+    unwatched = size(recommend_list,1);
+    for user_x = 1:size(user_list,1)
+        %a = find(user_list(:,1) == user_x);
+        user_id = user_list(user_x,1);
+        a = find(recommend_list(:,1) == user_id);
+        if ~isempty(a)
+            recommend_film = zeros(size(a,1),1);
+            disp(recommend_film);
+            s = a(1,1)+size(a,1)-1;
+            recommend_film(:,1) = recommend_list(a(1,1):s,2);
+            b = find(recommend_film(:,1) == user_list(user_x,2), 1);
+            if ~isempty(b)
+                unwatched = unwatched - 1;
+                if user_list(user_x,3) < threshold
+                    error = error + 1;
+                end
+            else
+                if user_list(user_x,3) >= threshold
+                    error = error + 1;
+                end
+            end
+        end
+    end
+    error = error + unwatched;
+    error_rate = error/size(recommend_list,1);
+end
---
+++ load_userrating.m
@@ -0,0 +1,3 @@
+data = sortrows(load('data/u.data'),1);
+save user_rating data;
+clear data;
